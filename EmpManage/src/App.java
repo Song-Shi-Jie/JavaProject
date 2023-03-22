890d19abc2c837cca71a31f3807f7659bee7fc6b
jay <3081251492@qq.com>
2023-03-22 11:12:10 +0800

    aa

--- EmpManage/src/App.java
+++ EmpManage/src/App.java
@@ -1,3 +1,13 @@
+package src;
+
+/*
+ * @Author: 宋世杰
+ * @Date: 2023-03-22 11:07:22
+ * @LastEditTime: 2023-03-22 11:11:42
+ * @LastEditors: 宋世杰
+ * @FilePath: \JavaProject\EmpManage\src\App.java
+ * @Description: 请自行修改描述
+ */
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
